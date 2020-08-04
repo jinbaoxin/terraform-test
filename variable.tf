0771a0e7cbade499effbbdd15870718bf5279aa2
Jbx <jinbaoxin@gmail.com>
2020-08-03 23:12:55 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,2 @@
-resource "azurerm_resource_group" "web_server_rg" {
-    name = var.web_server_rg
-    location = var.web_server_location
-}
+variable "web_server_location" {}
+variable "web_server_rg" {}
