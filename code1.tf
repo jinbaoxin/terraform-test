782ced79405479a80f57f8bcb59b3796d2f13cba
Jbx <jinbaoxin@gmail.com>
2020-08-03 23:11:01 -0400

    Update code1.tf

--- code1.tf
+++ code1.tf
@@ -1,5 +1,3 @@
-web_server_location         = "canadacentral"
-web_server_rg               = "web-rg-qa"
 resource "azurerm_resource_group" "web_server_rg" {
     name = var.web_server_rg
     location = var.web_server_location
